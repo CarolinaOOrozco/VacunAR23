45e9973ed3761691cd30f8664d8b1f619416e1f4
juan pablo <bolinojuan@gmail.com>
2023-10-04 18:47:59 -0300

    LaboratorioData

--- VacunAR23/src/AccesoDatos/LaboratorioData.java
+++ VacunAR23/src/AccesoDatos/LaboratorioData.java
@@ -5,10 +5,120 @@
  */
 package AccesoDatos;
 
+import entidades.Laboratorio;
+import java.sql.*;
+import java.util.*;
+
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author carol
  */
 public class LaboratorioData {
+private  Connection con=null;
+private ArrayList<Laboratorio> labo;
+    
+public LaboratorioData(){
+labo = new ArrayList<>();
+}
+
+
+public void nuevoLaboratorio(Laboratorio laboratorio){
+
+String sql = "INSERT INTO laboratorio(CUIT,nomLaboratorio, pais, domComercial,marca) VALUES(?,?,?,?)";
+    try {
+       PreparedStatement ps=con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+       ps.setInt(0, laboratorio.getCuit());
+       ps.setString(1, laboratorio.getNomLaboratorio());
+       ps.setString(2, laboratorio.getPais());
+       ps.setString(3,laboratorio.getDomComercial());
+       ps.setString(4,laboratorio.marca());
+      
+       ResultSet rs = ps.getGeneratedKeys();
+       
+       if(rs.next()){laboratorio.setCuit(rs.getInt(1));}
+
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "Error al acceder a la base"+ex.getMessage());
+    }
     
 }
+
+public void eliminarLaboratorioPorCuit(int cuit){
+    try {
+        String sql = "UPDATE laboratorio SET estado = 0 WHERE cuit = ? ";
+        
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(0,cuit);
+        int laboratorioEliminado = ps.executeUpdate();
+        if(laboratorioEliminado==1){
+        JOptionPane.showMessageDialog(null, "Laboratorio eliminado con exito");
+        }
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "Error al acceder a la base de datos");
+
+}
+}
+
+public void modificarLaboratorioPorCuit(Laboratorio lab){
+
+    try {
+        String sql ="UPDATE laboratorio.nomLaboratorio, laboratorio.pais,laboratorio.domComercial laboratorio.marca SET  = ?,?,?,? WHERE cuit = ?";
+        
+        PreparedStatement ps=con.prepareStatement(sql);
+      
+        ps.setString(0,lab.getNomLaboratorio());
+        ps.setString(1,lab.getPais());
+        ps.setString(2, lab.getDomComercial());
+        ps.setString(3,lab.marca());
+        ps.setInt(4, lab.getCuit());
+        
+        int laboratorioModificado = ps.executeUpdate();
+        if(laboratorioModificado==1){
+        JOptionPane.showMessageDialog(null, "Laboratorio modificado de manera exitosa");
+        }
+        
+    } catch (SQLException ex) {
+      JOptionPane.showMessageDialog(null, "Error al acceder a la base de datos"+ex.getMessage());
+    }
+
+
+}
+
+
+public ArrayList<Laboratorio> mostrarLaboratorio(){
+
+Laboratorio laboratorio;
+
+laboratorio = null;
+
+String sql = "SELECT * FROM laboratorio  WHERE estado = 1";
+    try {
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+        laboratorio.setCuit(rs.getInt("CUIT"));
+        laboratorio.setNomLaboratorio(rs.getString("nomLaboratorio"));
+        laboratorio.setPais(rs.getString("pais"));
+        laboratorio.setDomComercial(rs.getString("domComercial"));
+        laboratorio.setMarca(rs.getString("marca"));
+        labo.add(laboratorio);
+        }
+        
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "Error al acceder a la base de datos"+ex.getMessage());
+    }
+    
+    return labo;
+}
+
+}
+
+
+
+
+
+
+
+
--- VacunAR23/src/entidades/Laboratorio.java
+++ VacunAR23/src/entidades/Laboratorio.java
@@ -5,14 +5,23 @@ package entidades;
 public class Laboratorio {
 
     private  int cuit;
-    private String nomLaboratorio,pais,domComercial;
+    private String nomLaboratorio,pais,domComercial,marca;
 
-    public Laboratorio(int cuit, String nomLaboratorio, String pais, String domComercial) {
+    public Laboratorio(int cuit, String nomLaboratorio, String pais, String domComercial, String marca) {
         this.cuit = cuit;
         this.nomLaboratorio = nomLaboratorio;
         this.pais = pais;
         this.domComercial = domComercial;
+        this.marca=marca;
     }
+        public Laboratorio(String nomLaboratorio, String pais, String domComercial, String marca){
+        this.nomLaboratorio = nomLaboratorio;
+        this.pais = pais;
+        this.domComercial=domComercial;
+        this.marca=marca;
+         }
+    
+        public Laboratorio(){}
 
     public int getCuit() { return cuit;}    
     public void setCuit(int cuit) {this.cuit = cuit;}
@@ -32,7 +41,8 @@ public class Laboratorio {
     public void setDomComercial(String domComercial) {this.domComercial = domComercial;}
         
     
-
+    public String marca(){return marca;}
+    public void setMarca(String marca){this.marca=marca;}
     
     
 
